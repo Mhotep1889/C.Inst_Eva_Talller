1765389c243bd99017d1a9ece551dc5d941dac87
Mhotep1889 <felipe1889@hotmail.com>
2024-09-09 21:22:24 -0500

    Add files via upload

---
+++ ejercicio4/ejercicio4.py
@@ -0,0 +1,24 @@
+x, y, z, w = 15, 10, 20, 8
+
+if x < y < z or w % 2 == 0:
+    print(f"{x} esta entre {y} y {z} y {w} es par")
+else:
+    print(f"{x} no esta entre {y} y {z} y {w} no es par")
+
+a, b, c, d = 7, 5, 3, 3
+
+if a > b or c == d:
+    print(f"{a} es mayor que {b}")
+else:
+    print(f"{c} es igual a {d}")
+
+x, y, z = -3, 5, 0
+
+if x < 0 and y > 0 and z == 0:
+    print(f"{x} es negativo {y} es positivo y {z} es igual a cero")
+
+
+
+
+
+
